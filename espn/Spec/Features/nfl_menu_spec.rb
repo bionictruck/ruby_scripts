9f5ec24f6ab32488cb4e246313352edfea276e28
bionictruck <jaytrommer@charter.net>
2017-05-18 15:44:06 -0400

    Finishing top level browsing of the NFL page

--- espn/Spec/Features/nfl_menu_spec.rb
+++ espn/Spec/Features/nfl_menu_spec.rb
@@ -6,6 +6,7 @@ require './spec_helper.rb'
 
 ### The hover over feature does not appear to be implemented in this version of capybara
 ### Will add more tests for hover over once a solution has been found
+### The Tickets link opens another tab which selenium currently doesn't support
 
 feature "Navigate through all top NFL sub links" do
     scenario "Click on Home link and verify the appropriate page loads" do
@@ -38,25 +39,21 @@ feature "Navigate through all top NFL sub links" do
     scenario "Click on the Schedule link and verify the appropriate page loads" do
         if $driver.has_link?("Schedule")
             $driver.click_link("Schedule", :match => :first)
-            ### Title needs to be updated for each year
-            expect($driver).to have_title "2017 NFL Football Schedule"
+            expect($driver).to have_title "NFL Football Schedule"
         else
             $driver.click_link("More")
             $driver.click_link("Schedule", :match => :first)
-            ### Title needs to be updated for each year
-            expect($driver).to have_title "2017 NFL Football Schedule"
+            expect($driver).to have_title "NFL Football Schedule"
         end           
     end
     scenario "Click on the Standings link and verify the appropriate page loads" do
         if $driver.has_link?("Standings")
             $driver.click_link("Standings")
-            ### Title needs to be updated for each year
-            expect($driver).to have_title "2016 NFL Regular Season Standings - Nation Football League"
+            expect($driver).to have_title "NFL Regular Season Standings - Nation Football League"
         else
             $driver.click_link("More")
             $driver.click_link("Standings")
-            ### Title needs to be updated for each year
-            expect($driver).to have_title "2016 NFL Regular Season Standings - Nation Football League"
+            expect($driver).to have_title "NFL Regular Season Standings - Nation Football League"
         end
     end
     scenario "Click on the Stats links and verify the appropriate page loads" do
@@ -66,8 +63,7 @@ feature "Navigate through all top NFL sub links" do
         else
             $driver.click_link("More")
             $driver.click_link("Stats")
-            ### Title needs to be updated for each year
-            expect($driver).to have_title "2016 NFL Statistics - National Football League - ESPN"
+            expect($driver).to have_title "NFL Statistics - National Football League - ESPN"
         end
     end
     scenario "Click on the Teams link and verify the appropritate page loads" do
@@ -122,6 +118,161 @@ feature "Navigate through all top NFL sub links" do
             $driver.click_link("Draft")
             expect($driver).to have_title "Draft"
             $driver.click_link("NFL", :match => :first)
+        end
+    end
+    scenario "Click on the Playoff Machine link and verify the appropriate page loads" do
+        if $driver.has_link?("Playoff Machine")
+            $driver.click_link("Playoff Machine", :match => :first)
+            expect($driver).to have_title "Playoff Machine"
+            $driver.click_link("NFL", :match => :first)        
+        else
+            $driver.click_link("More")
+            $driver.click_link("Playoff Machine")
+            expect($driver).to have_title "Playoff Machine"
+            $driver.click_link("NFL", :match => :first)
+        end
+    end
+    scenario "Click on the Monday Night HQ link and verify the appropriate page loads" do
+        if $driver.has_link?("Monday Night HQ")
+            $driver.click_link("Monday Night HQ", :match => :first)
+            expect($driver).to have_title "Monday Night"
+            $driver.click_link("NFL", :match => :first)        
+        else
+            $driver.click_link("More")
+            $driver.click_link("Monday Night HQ")
+            expect($driver).to have_title "Monday Night"
+            $driver.click_link("NFL", :match => :first)
+        end
+    end    
+    scenario "Click on the Transactions link and verify the appropriate page loads" do
+        if $driver.has_link?("Transactions")
+            $driver.click_link("Transactions", :match => :first)
+            expect($driver).to have_title "NFL Football Transactions"
+            $driver.click_link("NFL", :match => :first)        
+        else
+            $driver.click_link("More")
+            $driver.click_link("Transactions")
+            expect($driver).to have_title "NFL Football Transactions"
+            $driver.click_link("NFL", :match => :first)
+        end
+    end
+    scenario "Click on the Expert Picks link and verify the appropriate page loads" do
+        if $driver.has_link?("Expert Picks")
+            $driver.click_link("Expert Picks", :match => :first)
+            expect($driver).to have_title "NFL Picks and Predictions"
+            $driver.click_link("NFL", :match => :first)        
+        else
+            $driver.click_link("More")
+            $driver.click_link("Expert Picks")
+            expect($driver).to have_title "NFL Picks and Predictions"
+            $driver.click_link("NFL", :match => :first)
+        end
+    end
+    scenario "Click on the Daily Lines link and verify the appropriate page loads" do
+        if $driver.has_link?("Daily Lines")
+            $driver.click_link("Daily Lines", :match => :first)
+            expect($driver).to have_title "NFL Odds and Daily Lines"
+            $driver.click_link("NFL", :match => :first)        
+        else
+            $driver.click_link("More")
+            $driver.click_link("Daily Lines")
+            expect($driver).to have_title "NFL Odds and Daily Lines"
+            $driver.click_link("NFL", :match => :first)
+        end
+    end
+    scenario "Click on the Pro Bowl link and verify the appropriate page loads" do
+        if $driver.has_link?("Pro Bowl")
+            $driver.click_link("Pro Bowl", :match => :first)
+            expect($driver).to have_title "NFL Pro Bowl"
+            $driver.click_link("NFL", :match => :first)        
+        else
+            $driver.click_link("More")
+            $driver.click_link("Pro Bowl")
+            expect($driver).to have_title "NFL Pro Bowl"
+            $driver.click_link("NFL", :match => :first)
+        end
+    end
+    scenario "Click on the Football Power Index link and verify the appropriate page loads" do
+        if $driver.has_link?("Football Power Index")
+            $driver.click_link("Football Power Index", :match => :first)
+            expect($driver).to have_title "Football Power Index"
+            $driver.click_link("NFL", :match => :first)        
+        else
+            $driver.click_link("More")
+            $driver.click_link("Football Power Index")
+            expect($driver).to have_title "Football Power Index"
+            $driver.click_link("NFL", :match => :first)
+        end
+    end
+    scenario "Click on the Injury Wire link and verify the appropriate page loads" do
+        if $driver.has_link?("Injury Wire")
+            $driver.click_link("Injury Wire", :match => :first)
+            expect($driver).to have_title "NFL Injury Wire"
+            $driver.click_link("NFL", :match => :first)        
+        else
+            $driver.click_link("More")
+            $driver.click_link("Injury Wire")
+            expect($driver).to have_title "NFL Injury Wire"
+            $driver.click_link("NFL", :match => :first)
+        end
+    end
+    scenario "Click on the Rumors link and verify the appropriate page loads" do
+        if $driver.has_link?("Rumors")
+            $driver.click_link("Rumors", :match => :first)
+            expect($driver).to have_title "NFL Rumor Central"
+            $driver.click_link("NFL", :match => :first)        
+        else
+            $driver.click_link("More")
+            $driver.click_link("Rumors")
+            expect($driver).to have_title "NFL Rumor Central"
+            $driver.click_link("NFL", :match => :first)
+        end
+    end
+    scenario "Click on the Chalk link and verify the appropriate page loads" do
+        if $driver.has_link?("Chalk")
+            $driver.click_link("Chalk", :match => :first)
+            expect($driver).to have_title "Chalk - ESPN"
+            $driver.click_link("NFL", :match => :first)        
+        else
+            $driver.click_link("More")
+            $driver.click_link("Chalk")
+            expect($driver).to have_title "Chalk - ESPN"
+            $driver.click_link("NFL", :match => :first)
+        end
+    end
+    scenario "Click on the PickCenter link and verify the appropriate page loads" do
+        if $driver.has_link?("PickCenter")
+            $driver.click_link("PickCenter", :match => :first)
+            expect($driver).to have_title "ESPN PickCenter - Picks, Analysis, Odds, Game Breakdowns"
+            $driver.click_link("NFL", :match => :first)        
+        else
+            $driver.click_link("More")
+            $driver.click_link("PickCenter")
+            expect($driver).to have_title "ESPN PickCenter - Picks, Analysis, Odds, Game Breakdowns"
+            $driver.click_link("NFL", :match => :first)
+        end
+    end
+    scenario "Click on the Coaches link and verify the appropriate page loads" do
+        if $driver.has_link?("Coaches")
+            $driver.click_link("Coaches", :match => :first)
+            expect($driver).to have_title "NFL Coaches"
+            $driver.click_link("NFL", :match => :first)        
+        else
+            $driver.click_link("More")
+            $driver.click_link("Coaches")
+            expect($driver).to have_title "NFL Coaches"
+            $driver.click_link("NFL", :match => :first)
+        end
+    end
+### We The Fans only has one link back to ESPN.com and that opens a separate tab
+    scenario "Click on the We The Fans link and verify the appropriate page loads" do
+        if $driver.has_link?("We The Fans")
+            $driver.click_link("We The Fans", :match => :first)
+            expect($driver).to have_title "We the Fans"        
+        else
+            $driver.click_link("More")
+            $driver.click_link("We The Fans")
+            expect($driver).to have_title "We the Fans"
         end
     end
 end
