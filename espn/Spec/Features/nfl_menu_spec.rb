bb200ad17b0393b3a108c94525036681f11a8c2e
bionictruck <jaytrommer@charter.net>
2017-05-18 13:51:03 -0400

    Maximizing browser and adding to nfl_menu_spec.rb

--- espn/Spec/Features/nfl_menu_spec.rb
+++ espn/Spec/Features/nfl_menu_spec.rb
@@ -10,43 +10,119 @@ require './spec_helper.rb'
 feature "Navigate through all top NFL sub links" do
     scenario "Click on Home link and verify the appropriate page loads" do
         $driver.click_link("NFL", :match => :first)
-        $driver.click_link("Home", :match => :first)
-        expect($driver).to have_title "NFL Football Teams, Scores, Stats, News, Standings, Rumors - National Football League - ESPN"
+        ### First we look to see if the link can be seen
+        if $driver.has_link?("Home")
+        ### If True then click on the Home link and verify the correct page loads
+            $driver.click_link("Home", :match => :first)
+            expect($driver).to have_title "NFL Football Teams, Scores, Stats, News, Standings, 
+            Rumors - National Football League - ESPN"
+        else
+            ### If False expand the More link and find the Home link in there.
+            $driver.click_link("More")
+            $driver.click_link("Home", :match => :first)
+            ### Verify the page loads
+            expect($driver).to have_title "NFL Football Teams, Scores, Stats, News, Standings, 
+            Rumors - National Football League - ESPN"
+        end
     end
     scenario "Click on the Scores link and verify the appropriate page loads" do
-        $driver.click_link("Scores")
-        expect($driver).to have_title "NFL Football Scores - NFL Scoreboard - ESPN"
-        ### At times the page refreshes the menu bar differently and some links seen on Home are no longer available
-        ### Click on Home bring the test back to the page where everything is seen
-        $driver.click_link("Home", :match => :first)
+        if $driver.has_link?("Scores")
+            $driver.click_link("Scores")
+            expect($driver).to have_title "NFL Football Scores - NFL Scoreboard - ESPN"
+        else
+            $driver.click_link("More")
+            $driver.click_link("Scores")
+            expect($driver).to have_title "NFL Football Scores - NFL Scoreboard - ESPN"
+        end
     end
     scenario "Click on the Schedule link and verify the appropriate page loads" do
-        $driver.click_link("Schedule", :match => :first)
-        ### Title needs to be updated for each year
-        expect($driver).to have_title "2017 NFL Football Schedule"
-        $driver.click_link("Home", :match => :first)
+        if $driver.has_link?("Schedule")
+            $driver.click_link("Schedule", :match => :first)
+            ### Title needs to be updated for each year
+            expect($driver).to have_title "2017 NFL Football Schedule"
+        else
+            $driver.click_link("More")
+            $driver.click_link("Schedule", :match => :first)
+            ### Title needs to be updated for each year
+            expect($driver).to have_title "2017 NFL Football Schedule"
+        end           
     end
     scenario "Click on the Standings link and verify the appropriate page loads" do
-        $driver.click_link("Standings")
-        ### Title needs to be updated for each year
-        expect($driver).to have_title "2016 NFL Regular Season Standings - Nation Football League"
-        $driver.click_link("Home", :match => :first)
+        if $driver.has_link?("Standings")
+            $driver.click_link("Standings")
+            ### Title needs to be updated for each year
+            expect($driver).to have_title "2016 NFL Regular Season Standings - Nation Football League"
+        else
+            $driver.click_link("More")
+            $driver.click_link("Standings")
+            ### Title needs to be updated for each year
+            expect($driver).to have_title "2016 NFL Regular Season Standings - Nation Football League"
+        end
     end
     scenario "Click on the Stats links and verify the appropriate page loads" do
-        $driver.click_link("Stats")
-        ### Title needs to be updated for each year
-        expect($driver).to have_title "2016 NFL Statistics - National Football League - ESPN"
-        $driver.click_link("Home", :match => :first)
+        if $driver.has_link?("Stats")
+            $driver.click_link("Stats")
+            expect($driver).to have_title "NFL Statistics - National Football League - ESPN"
+        else
+            $driver.click_link("More")
+            $driver.click_link("Stats")
+            ### Title needs to be updated for each year
+            expect($driver).to have_title "2016 NFL Statistics - National Football League - ESPN"
+        end
     end
     scenario "Click on the Teams link and verify the appropritate page loads" do
-        $driver.click_link("Teams")
-        expect($driver).to have_title "NFL Football Teams, National Football League Teams, Football Teams - ESPN"
-        $driver.click_link("Home", :match => :first)
+        if $driver.has_link?("Teams")
+            $driver.click_link("Teams", :match => :first)
+            expect($driver).to have_title "NFL Football Teams, National Football League Teams, Football Teams - ESPN"
+        else
+            $driver.click_link("More")
+            $driver.click_link("Teams")
+            expect($driver).to have_title "NFL Football Teams, National Football League Teams, Football Teams - ESPN"
+        end
+    end
+    scenario "Click on the Fantasy Football link and verify the appropritate page loads" do
+        if $driver.has_link?("Fantasy: Sign Up")
+            $driver.click_link("Fantasy: Sign Up", :match => :first)
+            expect($driver).to have_title "Fantasy Football"
+            $driver.click_link("NFL", :match => :first)
+        else
+            $driver.click_link("More")
+            $driver.click_link("Fantasy: Sign Up")
+            expect($driver).to have_title "Fantasy Football"
+            $driver.click_link("NFL", :match => :first)
+        end
     end
     scenario "Click on the Power Rankings link and verify the appropriate page loads" do
-        $driver.click_link("Power Rankings")
-        expect($driver).to have_title "Power Rankings"
-        $driver.click_link("Home", :match => :first)
+        if $driver.has_link?("Power Rankings")
+            $driver.click_link("Power Rankings")
+            expect($driver).to have_title "Power Rankings"
+        else
+            $driver.click_link("More")
+            $driver.click_link("Power Rankings")
+            expect($driver).to have_title "Power Rankings"
+        end
+    end
+    scenario "Click on the NFL Nation link and verify the appropriate page loads" do
+        if $driver.has_link?("NFL Nation")
+            $driver.click_link("NFL Nation", :match => :first)
+            expect($driver).to have_title "NFL Nation"
+        else
+            $driver.click_link("More")
+            $driver.click_link("NFL Nation")
+            expect($driver).to have_title "NFL Nation"
+        end
+    end
+    scenario "Click on the Draft link and verify the appropriate page loads" do
+        if $driver.has_link?("Draft")
+            $driver.click_link("Draft", :match => :first)
+            expect($driver).to have_title "Draft"
+            $driver.click_link("NFL", :match => :first)        
+        else
+            $driver.click_link("More")
+            $driver.click_link("Draft")
+            expect($driver).to have_title "Draft"
+            $driver.click_link("NFL", :match => :first)
+        end
     end
 end
 
--- espn/spec_helper.rb
+++ espn/spec_helper.rb
@@ -4,15 +4,18 @@ require 'capybara-webkit'
 require 'selenium-webdriver'
 require 'capybara/rspec'
 
-Capybara.register_driver :selenium do |app|
-  profile = Selenium::WebDriver::Firefox::Profile.new
-  profile.native_events = true
-  Capybara::Selenium::Driver.new(app, :browser => :firefox, profile: profile)
-end
+### For future use with ActionBuilder
+# Capybara.register_driver :selenium do |app|
+#   profile = Selenium::WebDriver::Firefox::Profile.new
+#   profile.native_events = true
+#   Capybara::Selenium::Driver.new(app, :browser => :firefox, profile: profile)
+# end
 
 Capybara.default_driver = :selenium
 Capybara.default_max_wait_time = 10
 Capybara.javascript_driver = :webkit
+Capybara.exact = false
+
 
 Capybara::Webkit.configure do |config|
     config.allow_url("")  
@@ -20,9 +23,5 @@ end
 
 $driver = Capybara::Session.new(:selenium)
 
-feature 'Navigating to ESPN' do
-    scenario "Verify the correct page is loaded" do
-        $driver.visit "http://www.espn.com"
-        expect($driver).to have_title "ESPN: The Worldwide Leader in Sports"
-    end
-end
+$driver.visit "http://www.espn.com"
+$driver.current_window.maximize
